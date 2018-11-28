8fa4b61f0f9bf8eb6e8494124ae9fcccbd6f4f40
Peter Konradi <codingchipmunk@gmail.com>
2018-11-28 10:23:27 +0100

    Changed beat main

--- beater/cameleventbeat.go
+++ beater/cameleventbeat.go
@@ -1,20 +1,19 @@
 package beater
 
 import (
-	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"time"
-
-	"github.com/codingchipmunk/JolokiaGo"
-	"github.com/codingchipmunk/JolokiaGo/jolokiaSSEStructs"
-	"github.com/codingchipmunk/JolokiaGo/responseValues"
 	"github.com/codingchipmunk/cameleventbeat/camelsse"
 	"github.com/codingchipmunk/cameleventbeat/config"
+	"github.com/codingchipmunk/jolokiago"
+	"github.com/codingchipmunk/jolokiago/events"
+	"github.com/codingchipmunk/jolokiago/java"
 	"github.com/r3labs/sse"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
 
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/common"
@@ -23,18 +22,16 @@ import (
 
 // Cameleventbeat configuration.
 type Cameleventbeat struct {
-	done    chan struct{}
-	config  config.Config
-	client  beat.Client
-	streams eventstreams
-}
-
-//	eventstreams holds the streams used for communication between the goroutines
-type eventstreams struct {
-	//	sse_events holds the sse events received by the sse client
-	sse_events chan *sse.Event
-	//	beat_events holds the beat events which have been generated from the sse events and are ready to be transmitted
-	beat_events chan beat.Event
+	stop             chan interface{}
+	config           config.Config
+	client           beat.Client
+	jolokia          jolokiago.Client
+	beatEvents       chan beat.Event
+	bean             java.MBean
+	logger           *logp.Logger
+	runningWorkers   *sync.WaitGroup
+	publisherRunning *sync.WaitGroup
+	listener         *events.Listener
 }
 
 // New creates an instance of cameleventbeat.
@@ -45,100 +42,127 @@ func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
 	}
 
 	bt := &Cameleventbeat{
-		done:   make(chan struct{}),
-		config: c,
-		streams: eventstreams{
-			sse_events:  make(chan *sse.Event),
-			beat_events: make(chan beat.Event),
+		config:     c,
+		beatEvents: make(chan beat.Event),
+		jolokia:    jolokiago.New(c.Jolokia.URL, &http.Client{Timeout: c.Jolokia.Timeout}),
+		bean: java.MBean{
+			Domain:  c.MBean.Domain,
+			Name:    c.MBean.Name,
+			Context: c.MBean.Context,
+			Type:    c.MBean.Type,
 		},
+		logger:           logp.NewLogger(b.Info.Name),
+		runningWorkers:   &sync.WaitGroup{},
+		publisherRunning: &sync.WaitGroup{},
+		stop:             make(chan interface{}),
 	}
+	bt.logger.Info("Initialized")
 	return bt, nil
 }
 
 // Run starts cameleventbeat.
-func (bt *Cameleventbeat) Run(b *beat.Beat) error {
-	logp.Info("cameleventbeat is running! Hit CTRL-C to stop it.")
+func (bt *Cameleventbeat) Run(b *beat.Beat) (err error) {
+	log := bt.logger
+	log.Info("Starting...")
 
-	var err error
+	//TODO Check if target mbean exists
+
 	bt.client, err = b.Publisher.Connect()
 	if err != nil {
 		return err
 	}
 
 	// Retrieve a client id
-	var client_id string
-	client_id, err = get_SSE_id(bt.config.Jolokia.URL)
+	bt.listener, err = bt.jolokia.NewSSEListener(http.DefaultClient)
 	if err != nil {
 		return err
 	}
 
-	logp.Info("Got client id: %s", client_id)
-	// Make a jolokiaClient MBean from the config MBean
-	client_MBean := jolokiaClient.MBean{
-		Domain:  bt.config.MBean.Domain,
-		Name:    bt.config.MBean.Name,
-		Context: bt.config.MBean.Context,
-		Type:    bt.config.MBean.Type,
+	log.Infof("Got client id: %s", bt.listener.ClientID())
+	rawEvents, started := bt.listener.StartListening()
+	if !started {
+		e := errors.New("listener did not start")
+		log.Error(e.Error())
+		return e
 	}
 
+	err = bt.listener.SubscribeToMBean(bt.bean)
+	if err != nil {
+		log.Errorf("Could not subscribe to MBean: %s", err)
+		return err
+	}
+	bt.publisherRunning.Add(1)
+	go publishEvents(bt.beatEvents, bt.client, bt.publisherRunning)
+	log.Debugf("Listening to URL: %s", bt.listener.FullURL())
+	log.Infof("Starting %d workers...", bt.config.Worker.Count)
+	for i := 1; i <= bt.config.Worker.Count; i++ {
+		bt.runningWorkers.Add(1)
+		go worker(rawEvents, bt.beatEvents, fmt.Sprintf("%s.%s%d", b.Info.Name, bt.config.Worker.Prefix, i), bt.runningWorkers)
+	}
+	log.Info("All workers running")
 	// Register for events
-	err = register_MBean_for_SSE(bt.config.Jolokia.URL, client_id, client_MBean)
-	if err != nil {
-		return err
-	}
 
-	//create sse client
-	client := sse.NewClient(bt.config.Jolokia.URL + "/notification/open/" + client_id + "/sse")
-	//launch sse client in seperate goroutine
-	go client.SubscribeChan("notification", bt.streams.sse_events)
-	go worker(bt.streams.sse_events, bt.done, bt.streams.beat_events, "worker-1")
-	publish_events(bt.streams.beat_events, bt.done, bt.client)
+	log.Info("Up and running! Hit CTRL-C (SIGINT) to gracefully stop.")
+	<-bt.stop
+
+	bt.listener.Stop()
+	bt.logger.Info("Listening stopped")
+	bt.logger.Info("Waiting for backlog being processed...")
+	bt.runningWorkers.Wait()
+	bt.logger.Info("All workers exited")
+	close(bt.beatEvents)
+	bt.logger.Info("Waiting for last events being published...")
+	bt.publisherRunning.Wait()
+	bt.logger.Info("All events published. Exiting now.")
+	close(bt.stop)
 	return nil
 }
 
 // Stop stops cameleventbeat.
 func (bt *Cameleventbeat) Stop() {
+	bt.logger.Info("SIGINT received, stopping...")
+	bt.stop <- true
+	<-bt.stop
 	bt.client.Close()
-	close(bt.done)
 }
 
-func publish_events(beat_events <-chan beat.Event, done <-chan struct{}, client beat.Client) {
+func publishEvents(beatEvents <-chan beat.Event, client beat.Client, running *sync.WaitGroup) {
+	defer running.Done()
 	for {
-		select {
-		case <-done:
+		event, ok := <-beatEvents
+		if ok {
+			client.Publish(event)
+		} else {
 			return
-		case event := <-beat_events:
-			{
-				client.Publish(event)
-			}
 		}
 	}
 }
 
-func worker(sse_events <-chan *sse.Event, done <-chan struct{}, beat_events chan<- beat.Event, worker_id string) error {
-	var event *sse.Event
+func worker(sseEvents <-chan *sse.Event, beatEvents chan<- beat.Event, workerID string, running *sync.WaitGroup) {
+	defer running.Done()
+	log := logp.NewLogger(workerID)
+	log.Debugf("Running and waiting for work...")
+
 	for {
-		select {
-		case <-done:
-			return nil
-		case event = <-sse_events:
-			{
-				beat_event, err := make_beat_event(event, worker_id)
-				if err != nil {
-					logp.Err("Worker %s encountered an error while trying to make a beat event: %s", worker_id, err)
-				} else {
-					beat_events <- beat_event
-				}
+		event, ok := <-sseEvents
+		if ok {
+			beatEvent, err := makeBeatEvent(event, workerID)
+			if err != nil {
+				log.Errorf("Encountered an error while trying to make a beat event: %s", err)
+				log.Debug(event)
+			} else {
+				beatEvents <- beatEvent
 			}
-
+		} else {
+			break
 		}
 	}
-
+	log.Debugf("Channel closed, exiting...")
 }
 
-func unmarshall_sse_event(sse_event *sse.Event) (jolokiaSSEStructs.SSERoot, error) {
-	var root jolokiaSSEStructs.SSERoot
-	err := json.Unmarshal(sse_event.Data, &root)
+func unmarshalSSEEvent(sseEvent *sse.Event) (events.EventData, error) {
+	var root events.EventData
+	err := json.Unmarshal(sseEvent.Data, &root)
 	if err != nil {
 		return root, err
 	}
@@ -146,8 +170,8 @@ func unmarshall_sse_event(sse_event *sse.Event) (jolokiaSSEStructs.SSERoot, erro
 	return root, nil
 }
 
-func make_beat_event(sse_event *sse.Event, worker_id string) (beat.Event, error) {
-	root, err := unmarshall_sse_event(sse_event)
+func makeBeatEvent(sseEvent *sse.Event, workerID string) (beat.Event, error) {
+	root, err := unmarshalSSEEvent(sseEvent)
 	if err != nil {
 		return beat.Event{}, err
 	}
@@ -160,112 +184,98 @@ func make_beat_event(sse_event *sse.Event, worker_id string) (beat.Event, error)
 
 	event := beat.Event{
 		Timestamp: data.TimeStamp,
-		Meta: common.MapStr{
-			"worker_id":       worker_id,
-			"jolokia_time":    root.Notifications[0].TimeStamp.Time,
-			"event_processed": time.Now(),
-		},
 		Fields: common.MapStr{
-			"endpoint_uri":      data.EndpointURI,
-			"exchange_id":       data.ExchangeID,
-			"breadcrumb":        data.Headers.Breadcrumbid,
-			"body":              data.Body,
-			"firedtime":         data.Headers.Firedtime.Time,
-			"camel_to_endpoint": data.Properties.ToEndpoint,
-			"camel_timer_name":  data.Properties.CamelTimer.Name,
+			"beat": common.MapStr{
+				"workerid":        workerID,
+				"event_processed": time.Now().UnixNano(),
+			},
 		},
+	}
+	cmlMap, e := getCamelMap(data)
+	if e {
+		event.Fields.Put("camel", cmlMap)
 	}
 	return event, nil
 }
 
-//	Register the client_ID at the jolokia agent for sse-events for the MBean
-func register_MBean_for_SSE(agent_url string, client_ID string, bean jolokiaClient.MBean) error {
-	var err error
-
-	register_request := jolokiaClient.RegisterEventRequest{Mode: "sse", ClientID: client_ID, BaseRequest: jolokiaClient.BaseRequest{Type: "notification", Command: "add", MBean: &bean}}
-
-	var resp_root jolokiaClient.ResponseRoot
-	resp_root, err = make_Jolokia_Request(agent_url, register_request)
-	if err != nil {
-		return err
+func getCamelMap(data camelsse.CamelSSEData) (mp common.MapStr, notEmpty bool) {
+	mp = map[string]interface{}{}
+	if data.EndpointURI != "" {
+		mp.Put("endpoint_uri", data.EndpointURI)
+		notEmpty = true
+	}
+	if data.ExchangeID != "" {
+		mp.Put("exchange_id", data.ExchangeID)
+		notEmpty = true
+	}
+	if !data.Headers.Firedtime.Time.IsZero() {
+		mp.Put("firedtime", data.Headers.Firedtime.Time.UnixNano())
+		notEmpty = true
 	}
 
-	//Check if http-status is 200, throw an error if not
-	if resp_root.Status != 200 {
-		return errors.New("The http-request to the jolokia-Agent to register for MBean notifications was successfull, however the status-code embedded in the response is not 200 but " + string(resp_root.Status))
+	if data.Properties.ToEndpoint != "" {
+		mp.Put("to_endpoint", data.Properties.ToEndpoint)
+		notEmpty = true
 	}
-
-	return nil
+	eventMP, e := getEventMap(data)
+	if e {
+		mp.Put("event", eventMP)
+		notEmpty = true
+	}
+	timrMP, e := getTimerMap(data)
+	if e {
+		mp.Put("timer", timrMP)
+		notEmpty = true
+	}
+	return
+}
+func getEventMap(data camelsse.CamelSSEData) (mp common.MapStr, empty bool) {
+	mp = map[string]interface{}{}
+	if data.Headers.Breadcrumbid != "" {
+		mp.Put("breadcrumb", data.Headers.Breadcrumbid)
+		empty = true
+	}
+	if data.Body != "" {
+		mp.Put("body", data.Body)
+		empty = true
+	}
+	return
 }
 
-//	Requests the Jolokia agent on the given url to recieve a client id which is needed to register for sse requests
-func get_SSE_id(agent_url string) (string, error) {
-	var err error
-
-	//Make a request to jolokia to get a client id
-	var resp_root jolokiaClient.ResponseRoot
-	resp_root, err = make_Jolokia_Request(agent_url, jolokiaClient.BaseRequest{Type: "notification", Command: "register"})
-	if err != nil {
-		return "", err
+func clean(mappings common.MapStr) (mp common.MapStr, notEmpty bool) {
+	for k, v := range (mappings) {
+		if v != "" {
+			mp.Put(k, v)
+			notEmpty = true
+		}
 	}
-
-	//Check if http-status is 200, throw an error if not
-	if resp_root.Status != 200 {
-		return "", errors.New("The http-request to the jolokia-Agent to retrieve a client-id was successfull, however the status-code embedded in the response is not 200 but " + string(resp_root.Status))
-	}
-	//Check if Value-Field is nil
-	if resp_root.Value == nil {
-		return "", errors.New("The http-request to the jolokia-Agent to retrieve a client-id was successfull, however there was no field labeled \"value\" in the response")
-	}
-
-	//Unmarshall Value-Field
-	var sse_value responseValues.ResponseValue
-	err = json.Unmarshal(resp_root.Value, &sse_value)
-	if err != nil {
-		return "", err
-	}
-
-	//Check if id field is empty
-	if sse_value.Id == "" {
-		return "", errors.New("The http-request to the jolokia-Agent to retrieve a client-id was successfull, however the id field was empty")
-	}
-
-	return sse_value.Id, nil
+	return
 }
 
-//	make_Jolokia_Request makes an request to the jolokia agent specified in agent_url.
-//	The request is marshaled into JSON and transmitted to the jolokia agent in a POST request.
-//	Afterwards the response is unmarshaled into a ResponseRoot struct and returned
-func make_Jolokia_Request(agent_url string, request interface{}) (jolokiaClient.ResponseRoot, error) {
-	var err error
-
-	//Marshall the request
-	var js []byte
-	js, err = json.Marshal(request)
-	if err != nil {
-		return jolokiaClient.ResponseRoot{}, err
+func getTimerMap(data camelsse.CamelSSEData) (mp common.MapStr, notEmpty bool) {
+	mp = map[string]interface{}{}
+	tmr := data.Properties.CamelTimer
+	if tmr.Name != "" {
+		mp.Put("name", tmr.Name)
+		notEmpty = true
 	}
 
-	//Transmit the request to the jolokia Client
-	var httpResp *http.Response
-	httpResp, err = http.Post(agent_url, "application/json", bytes.NewReader(js))
-	if err != nil {
-		return jolokiaClient.ResponseRoot{}, err
+	if tmr.Counter != "" {
+		number, _ := strconv.Atoi(tmr.Counter)
+		mp.Put("counter", number)
+		notEmpty = true
 	}
 
-	//Close the response body so it can be read safely
-	defer httpResp.Body.Close()
-	//Read the response body
-	var httpBody []byte
-	httpBody, err = ioutil.ReadAll(httpResp.Body)
-	if err != nil {
-		return jolokiaClient.ResponseRoot{}, err
+	if tmr.Period != "" {
+		number, _ := strconv.Atoi(tmr.Period)
+		mp.Put("period", number)
+		notEmpty = true
 	}
 
-	//Unmarshall the response body into the ResponseRoot struct
-	var jolokia_response jolokiaClient.ResponseRoot
-	err = json.Unmarshal(httpBody, &jolokia_response)
+	if !tmr.FiredTime.Time.IsZero() {
+		mp.Put("firedtime", tmr.FiredTime.Time.UnixNano())
+		notEmpty = true
+	}
 
-	return jolokia_response, err
-
+	return
 }
